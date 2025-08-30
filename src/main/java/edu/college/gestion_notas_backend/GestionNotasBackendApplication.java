8f0a515d225ca5916539cd9229e96232d31fd9fd
JBriam <jbrayan14@gmail.com>
2025-08-30 02:43:51 -0500

    Primer commit

---
+++ src/main/java/edu/college/gestion_notas_backend/GestionNotasBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.college.gestion_notas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionNotasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionNotasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/college/gestion_notas_backend/GestionNotasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.college.gestion_notas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionNotasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
