b02d7018565da7aa26355722ce3329b724ba21f5
JBriam <jbrayan14@gmail.com>
2025-09-30 12:26:25 -0500

    Controlador de docente

--- src/main/java/edu/college/gestion_notas_backend/controller/DocenteController.java
+++ src/main/java/edu/college/gestion_notas_backend/controller/DocenteController.java
@@ -1,5 +1,22 @@
 package edu.college.gestion_notas_backend.controller;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import edu.college.gestion_notas_backend.dto.request.CrearDocenteDTO;
 import edu.college.gestion_notas_backend.dto.response.DocenteResponseDTO;
 import edu.college.gestion_notas_backend.model.Docente;
@@ -8,13 +25,6 @@ import edu.college.gestion_notas_backend.service.DocenteService;
 import edu.college.gestion_notas_backend.service.UsuarioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/docentes")
